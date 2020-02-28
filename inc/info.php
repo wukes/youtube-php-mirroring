b6af1e5edeefc792ce2359a269da9683b84dffea
acghero <61540218+acghero@users.noreply.github.com>
2020-02-28 14:18:38 +0000

    Update info.php

--- inc/info.php
+++ inc/info.php
@@ -1,15 +1,11 @@
 <?php
 //
-$ym='http://'.$_SERVER['SERVER_NAME'].'/';//你的站点域名。不可为空，否则解析器无法工作
+$ym='http://acgfuck.herokuapp.com/'.$_SERVER['SERVER_NAME'].'/';//你的站点域名。不可为空，否则解析器无法工作
 $parser='parser';//解析器目录,默认parser，如果本程序不是安装在根目录请加上上级路径，如abc/parser
 $home_v='UCcLK3j-XWdGBnt5bR9NJHaQ';//首页推荐视频用户身份标示
-$youtube_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//youtube视频API
-$gurl_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//谷歌缩址api，用于防止dns污染
-$title='Youtube直通车';//您的网站名字
-
-//seo设置
-$keyw_ords='Youtube镜像,google,youtube,镜像';//keywords头部关键词设置
-$descr_iption='不翻墙，看视频';//description网站描述
+$youtube_api='AIzaSyC4Ai1IyXP3EXYyetpRrmpMdvNp1kPe_9M';//youtube视频API
+$gurl_api='AIzaSyCx9Du_moeT4NjE2wTOY2Jb_OpLacGGj3s';//谷歌缩址api，用于防止dns污染
+$title='直通车';//您的网站名字
 
 
 ?>
